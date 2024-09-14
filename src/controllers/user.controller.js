6932bb335ea35007a92c23b44dc480692ac0cc54
vanshsharma <vanshsharma9933@gmail.com>
2024-09-15 01:16:51 +0530

    fix all register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,6 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async(req,res) => {
     const {fullname, email, username , password} = req.body
-
     if(fullname == ""){
         throw new ApiError(400, "fullname is required")
     }
@@ -15,7 +14,7 @@ const registerUser = asyncHandler( async(req,res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username},{email}]
     })
 
@@ -25,6 +24,12 @@ const registerUser = asyncHandler( async(req,res) => {
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
     const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    // let coverImageLocalPath;
+    // if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    //     coverImageLocalPath = req.files.coverImage[0].path;
+    // }
+
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar is required")
@@ -38,7 +43,6 @@ const registerUser = asyncHandler( async(req,res) => {
     }
 
     const user = await User.create({fullname, avatar: avatar.url, coverImage: coverImage?.url || "", email, username: username.toLowerCase(), password})
-
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
--- src/db/index.js
+++ src/db/index.js
@@ -3,11 +3,11 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`)
 
         console.log(`\n MongoDB connected: DB HOST: ${connectionInstance.connection.host} \n`)
     } catch (error) {
-       console.log("Error: ", error); 
+       console.log("Error: ", error);  
        process.exit(1);
     }
 }
