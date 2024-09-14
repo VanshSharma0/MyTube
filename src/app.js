69937fb9b8d64531dfc555eb07a9fb3051243c8e
vanshsharma <vanshsharma9933@gmail.com>
2024-09-15 00:05:52 +0530

    add register controller

--- src/app.js
+++ src/app.js
@@ -13,3 +13,4 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 import express from 'express';
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
 
 dotenv.config({ path: './env' });
 
 const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
 
 connectDB()
 .then(() => {
